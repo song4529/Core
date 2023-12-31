fcd3a59d4a202b204cfaa5bf96e3e136ec401228
song4529 <gusdn4529@gmail.com>
2023-12-31 23:53:35 +0900

    feat : SingletonService 테스트 구현

--- src/test/java/hello/core/singleton/SingletonService.java
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -1,5 +1,10 @@
 package hello.core.singleton;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class SingletonService {
     //1. static 영역에 객체를 딱 1개만 생성해둔다.
     private static final SingletonService instance = new SingletonService();
@@ -12,5 +17,21 @@ public class SingletonService {
     }
     public void logic() {
         System.out.println("싱글톤 객체 로직 호출");
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest() {
+        //private으로 생성자를 막아두었다. 컴파일 오류가 발생한다. //new SingletonService();
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+        //2. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+        //참조값이 같은 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+        // singletonService1 == singletonService2
+        assertThat(singletonService1).isSameAs(singletonService2);
+        singletonService1.logic();
     }
 }
